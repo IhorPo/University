1d42ab65b9c5343554b8fb4661c903604f9e2369
LiashenkoAndrey <a.liashenko@e-u.edu.ua>
2023-11-16 12:54:39 +0200

    refactor methods

--- university/src/main/java/com/project_1/university/service/StudentService.java
+++ university/src/main/java/com/project_1/university/service/StudentService.java
@@ -1,52 +1,56 @@
 package com.project_1.university.service;
 
 import com.project_1.university.dataAccessLayer.StudentRepository;
-import com.project_1.university.models.StudentPojo;
+import com.project_1.university.exceptions.ServiceException;
+import com.project_1.university.models.Student;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class StudentService {
+
+    private static final Logger logger = LogManager.getLogger(StudentService.class);
+
     private final StudentRepository repository;
     @Autowired
     public StudentService(StudentRepository repository) {
         this.repository = repository;
     }
 
-    public List<StudentPojo> getAll(){
+    public List<Student> getAll(){
         return repository.findAll();
     }
 
-    public StudentPojo findById(Integer id){
+    public Student findById(Integer id){
         return repository.findById(id).orElseThrow(
                 () -> new NoSuchElementException("No such student")
         );
     }
 
-    public StudentPojo add(StudentPojo student){
-        return repository.save(student);
+    public Student add(Student student){
+        try {
+            return repository.save(student);
+        } catch (Exception e) {
+            logger.error(e);
+            throw new ServiceException(e);
+        }
     }
 
-    public StudentPojo update(StudentPojo student){
-        Optional<StudentPojo> student1 = repository.findById(student.getId());
-        if(student1.isPresent()){
-            StudentPojo student2 = student1.get();
-            student2.setName(student.getName());
-            student2.setSurname(student.getSurname());
-            student2.setEmail(student.getEmail());
-            student2.setStudentGroup(student.getStudentGroup());
-            return repository.save(student2);
-        }else {
+    public Student update(Student student){
+        if (repository.existsById(student.getId())) {
+            return repository.save(student);
+        } else {
             throw new NoSuchElementException("No such student");
         }
     }
 
     public void delete(Integer id){
-        StudentPojo student = repository.findById(id).orElseThrow(
+        Student student = repository.findById(id).orElseThrow(
                 () ->new NoSuchElementException("No such student")
         );
         repository.delete(student);
